5d7c9f110d39359158681d6065331612dd2d4680
Chenyang Huang <hchenyang@hotmail.com>
2018-03-28 16:52:56 -0600

    update

--- traditional_NLP.py
+++ traditional_NLP.py
@@ -2,20 +2,17 @@ import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit, ShuffleSplit
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 
 
 def cbet_data(label_cols):
     data = pd.read_csv('data/CBET.csv')
-    # test_data = pd.read_csv('data/test.csv')
-    # label = pd.read_csv('data/test.csv')
-    from nltk.corpus import stopwords
-    from nltk.tokenize import word_tokenize
     label = data[label_cols]
-    # for col in label_cols:
-    #     label.append(data[col])
-    #
-    # label = np.asarray(label).transpose()
-    # example_sent = "This is a sample sentence, showing off the stop words filtration."
 
     stop_words = set(stopwords.words('english'))
 
@@ -26,14 +23,6 @@ def cbet_data(label_cols):
         filtered_sentence = [w for w in word_tokens if not w in stop_words]
         train_text.append(' '.join(filtered_sentence))
 
-    # test_text = []
-    # for t in test_data['comment_text'].fillna("fillna").values:
-    #     t = t.lower()
-    #     word_tokens = word_tokenize(t)
-    #     filtered_sentence = [w for w in word_tokens if not w in stop_words]
-    #     test_text.append(' '.join(filtered_sentence))
-    #
-    # sub_id = test_data['id']
     return train_text, label
 
 
@@ -41,26 +30,29 @@ if __name__ == '__main__':
     label_cols = ['anger', 'fear', 'joy', 'love', 'sadness', 'surprise', 'thankfulness', 'disgust', 'guilt']
     X, y = cbet_data(label_cols)
 
-    # if True:
-    #     X, y, X_test, sub_id = cbet_data(label_cols)
-    #     with open('tmp', 'bw') as f:
-    #         pickle.dump([X, y, X_test, sub_id], f)
-    # else:
-    #     with open('tmp', 'br') as f:
-    #         X, y, X_test, sub_id = pickle.load(f)
-
-    # import pickle
-    # with open('tmp', 'bw') as f:
-    #     pickle.dump([X_train, X_test, y_train, y_test], f)
-
-
     sss = ShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
-    # golds = np.zeros((int(len(X)*0.1) + 1, y.shape[1]))
-    # preds = np.zeros((int(len(X)*0.1) + 1, y.shape[1]))
     y = np.asarray(y[label_cols])
     train_index, dev_index = next(sss.split(X, y))
     X_train, X_dev = [X[i] for i in train_index], [X[i] for i in dev_index]
     y_train, y_dev = y[train_index], y[dev_index]
 
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    bag_of_words_len = 5000
+    vectorizer = CountVectorizer(analyzer="word", tokenizer=None,
+                                 preprocessor=None,
+                                 max_features=bag_of_words_len)
 
+    x_train_fea = vectorizer.fit_transform(X_train)
+    x_train_fea = x_train_fea.toarray()
+    x_dev_fea = vectorizer.transform(X_dev)
+    x_dev_fea = x_dev_fea.toarray()
+
+    # clf = RandomForestClassifier(n_estimators=30, random_state=0, n_jobs=-1)
+    clf = OneVsRestClassifier(SVC(kernel='linear'), n_jobs=-1)
+
+    clf.fit(x_train_fea, y_train)
+    y_dev_pred = clf.predict(x_dev_fea)
+    f1 = f1_score(y_dev, y_dev_pred, average='macro')
+    p = precision_score(y_dev, y_dev_pred, average='macro')
+    r = recall_score(y_dev, y_dev_pred, average='macro')
+
+    print(f1, p, r)
